27b8277d3a35ded14e776bc48e79ddb89538ba56
Miguel Lara <miguel.larasj@gmail.com>
2024-05-11 13:14:37 -0500

    a simple hook

--- hooks/lib/views/counter.dart
+++ hooks/lib/views/counter.dart
@@ -1,33 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class CounterView extends StatefulWidget {
+class CounterView extends HookWidget {
   const CounterView({super.key});
-
-  @override
-  State<CounterView> createState() => _CounterViewState();
-}
-
-class _CounterViewState extends State<CounterView> {
-  int _counter = 0;
-
+  
   @override
   Widget build(BuildContext context) {
+    final counter = useState<int>(0);
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: Text(
-          '$_counter',
+          '${counter.value}',
           style: const TextStyle(fontSize: 30),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          setState(() {
-            _counter++;
-          });
+          counter.value++;
         },
       ),
     );
   }
+
 }
