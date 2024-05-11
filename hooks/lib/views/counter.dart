fcb880ca3e67dc1cbe2f6e39156714bcec4b9c7a
Miguel Lara <miguel.larasj@gmail.com>
2024-05-11 13:21:39 -0500

    Another small hook

--- hooks/lib/views/counter.dart
+++ hooks/lib/views/counter.dart
@@ -3,25 +3,28 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 class CounterView extends HookWidget {
   const CounterView({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     final counter = useState<int>(0);
+    final numbers = useState<List<int>>([]);
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text(
-          '${counter.value}',
-          style: const TextStyle(fontSize: 30),
-        ),
+      body: ListView.builder(
+        itemCount: numbers.value.length,
+        itemBuilder: ((context, index) {
+          return ListTile(
+            title: Text(numbers.value[index].toString()),
+          );
+        }),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          counter.value++;
+          numbers.value = [...numbers.value, numbers.value.length + 1];
+          // counter.value++;
         },
       ),
     );
   }
-
 }
