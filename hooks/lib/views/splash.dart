06ac48f5cc6af69bfeb4ec45b2abda13f4ce2bab
Miguel Lara <miguel.larasj@gmail.com>
2024-05-11 23:32:07 -0500

    the useEffect hook

--- hooks/lib/views/splash.dart
+++ hooks/lib/views/splash.dart
@@ -1,37 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 import '../routes.dart';
 
-class SplashView extends StatefulWidget {
+class SplashView extends HookWidget {
   const SplashView({super.key});
 
   @override
-  State<SplashView> createState() => _SplashViewState();
-}
-
-class _SplashViewState extends State<SplashView> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback(
-      (_) => _init(),
-    );
-  }
-
-  Future<void> _init() async {
-    await Future.delayed(
-      const Duration(seconds: 1),
-    );
-    if (mounted) {
-      Navigator.pushReplacementNamed(
-        context,
-        Routes.home,
-      );
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
+    // the useEffect hook works as an initState function
+    // it executes the effect function on every build or when the keys change
+    useEffect(
+      // this is the effect function
+      () {
+        Future<void> init() async {
+          await Future.delayed(
+            const Duration(seconds: 1),
+          );
+          if (context.mounted) {
+            Navigator.pushReplacementNamed(
+              context,
+              Routes.home,
+            );
+          }
+        }
+
+        WidgetsBinding.instance.addPostFrameCallback(
+          (_) => init(),
+        );
+        // the function in return executes on 
+        return null;
+      },
+      // the useEffect hook will execute only once if it's the secound parameter
+      // is set to [].
+      // if we add keys the hook will execute again when the key value changes 
+      // list of keys... [] => [key1, key2]
+      [],
+    );
     return const Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
